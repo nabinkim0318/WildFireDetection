a5e920f1365ecad14161686f143e1978e887e08d
nabinkim0318 <32893645+nabinkim0318@users.noreply.github.com>
2022-03-07 22:35:26 -0500

    More OOP approach

--- BDBI_Wildfire_dataset.py
+++ BDBI_Wildfire_dataset.py
@@ -1,3 +1,7 @@
+"""
+    Converts NOAA precipitation data from text table to csv. See:
+    https://www.cnrfc.noaa.gov/data/text/precip_google/PNM_Mar_2022.txt
+"""
 import requests
 import pandas as pd
 TABLE_START = 4
@@ -5,29 +9,42 @@ ROW_START = 55
 START_YEAR = 2000
 END_YEAR = 2022
 
-# Converting each url to text files
-
+df = pd.DataFrame()
 firstPartLink = "https://www.cnrfc.noaa.gov/data/text/precip_google/PNM"
 Month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-df = pd.DataFrame()
-
-for year in range (START_YEAR, END_YEAR + 1):
-    for month in Month:
-        monYear = ("_" + month + "_" + str(year))
-        linkName = (firstPartLink + monYear + ".txt")
-        
-        data = requests.get(linkName)   # response object = contents 
-        content = data.text
-        lines = content.splitlines()[TABLE_START:]
-        data = [line[ROW_START:].split() for line in lines]
-
-        df2 = pd.DataFrame(data)
-        df2 = df2.reset_index()
-        df2.columns = df2.iloc[0]
-        df2 = df2[1:]
-        df2['Month'] = month
-        df2['Year'] = year
-        df = pd.concat([df, df2], ignore_index = True)
-df.to_csv("data_noaa_precip.csv")
 
 
+def generate_urls(month, year):
+    monYear = ("_" + month + "_" + str(year))
+    url = (firstPartLink + monYear + ".txt")
+    return url
+
+
+def get_precipitation_txt_from_url(url):
+    data = requests.get(url)   # response object = contents 
+    content = data.text
+    return content
+
+
+def precipitation_txt_to_df(content, df, month, year):
+    lines = content.splitlines()[TABLE_START:]
+    data = [line[ROW_START:].split() for line in lines]
+
+    df2 = pd.DataFrame(data)
+    df2 = df2.reset_index()
+    df2.columns = df2.iloc[0]
+    df2 = df2[1:]
+    df2['Month'] = month
+    df2['Year'] = year
+    df = pd.concat([df, df2], ignore_index = True)
+    return df
+
+def generate_csv(df, START_YEAR, END_YEAR):
+    for year in range (START_YEAR, END_YEAR + 1):
+        for month in Month:
+            url = generate_urls(month, year)
+            content = get_precipitation_txt_from_url(url)
+            df = precipitation_txt_to_df(content, df, month, year)
+    df.to_csv("data_noaa_precip.csv")
+
+generate_csv(df, START_YEAR, END_YEAR)
